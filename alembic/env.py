af624531b898b436f1894a3aa15b35b202da866a
42jasuh <diqkqkqk@yahoo.com>
2025-04-04 01:07:15 +0900

    chore: 불필요한 Alembic 마이그레이션 파일 정리

--- alembic/env.py
+++ alembic/env.py
@@ -1,43 +1,43 @@
 from logging.config import fileConfig
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import create_engine
 from alembic import context
 from app.core.config import settings
+from app.db.session import Base
+
+from app.models.choice import Choice
+from app.models.question import Question
+from app.models.quiz import Quiz
 from app.models.user import User
-import asyncio
 
 config = context.config
 fileConfig(config.config_file_name) if config.config_file_name else None
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
-target_metadata = User.metadata
+engine = create_engine(settings.DATABASE_URL, echo=True)
+target_metadata = Base.metadata
 
 def run_migrations_offline():
     """오프라인 모드 (동기 방식)"""
     context.configure(
-        url=settings.DATABASE_URL,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_type=True,
+        compare_server_default=True,
     )
     with context.begin_transaction():
         context.run_migrations()
 
-async def run_migrations_online():
-    """온라인 모드 (비동기 방식)"""
-    async with engine.connect() as connection:
-        # 1. 컨텍스트에 연결 및 메타데이터 설정
-        def run_migrations(conn):
-            context.configure(
-                connection=conn, 
-                target_metadata=target_metadata
-            )
-            with context.begin_transaction():
-                context.run_migrations()
+def run_migrations_online():
+    """온라인 모드 (동기 방식)"""
+    with engine.connect() as connection:
+        context.configure(                
+            connection=connection,
+            target_metadata=target_metadata,
+            dialect_opts={"paramstyle": "named"},
+            compare_type=True,
+            compare_server_default=True
+        )
+        with context.begin_transaction():
+            context.run_migrations()
 
-        # 2. 동기 함수를 비동기로 실행
-        await connection.run_sync(run_migrations)
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    asyncio.run(run_migrations_online())
+run_migrations_online()
