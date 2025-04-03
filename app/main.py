e84a2d3046483e540d6de6a23942eb41ef031556
42jasuh <diqkqkqk@yahoo.com>
2025-04-04 01:06:00 +0900

    chore: FastAPI 메인 애플리케이션 설정 수정

---
+++ app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+
+from app.api.v1.router import router
+from app.db.session import Base
+from app.db.session import engine
+
+app = FastAPI(
+    title="SJH_Quiz",
+    description="Project",
+    version="1.0.0"
+)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    
+    openapi_schema = get_openapi(
+        title="SJH API",
+        version="1.0.0",
+        description="JWT Token을 사용한 인증 방식",
+        routes=app.routes,
+    )
+    
+    openapi_schema.setdefault("components", {}).setdefault("securitySchemes", {})[""] = {
+    }
+    
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
+
+app.include_router(router, prefix="/api/v1")
